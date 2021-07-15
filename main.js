9d05df4455bf5c352d1b153fd72fbcd0b68e41d6
Fernando Tavares <fernando.adriano.tavares@gmail.com>
2021-07-15 14:34:31 -0300

    :tada: Refinamento do código

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-/***/
+/*
+Projeto
+*/
 import "./src/styles/settings/colors.css";
 import "./src/styles/generic/generic.css";
 import "./src/styles/elements/base.css";
-
-import CardGame from "./src/components/CardGame";
+import BoardGame from "./src/objects/BoardGame";
+import PlayerName from "./src/components/PlayerName";
 
 const $root = document.querySelector("#root");
 
-const $htmlCardGame = CardGame();
-
-$root.insertAdjacentHTML("beforeend", $htmlCardGame);
-
+$root.insertAdjacentHTML(
+  "beforeend",
+  `
+${PlayerName("Player1")}
+${PlayerName("Player2")}
+${BoardGame(6)}
+`
+);
