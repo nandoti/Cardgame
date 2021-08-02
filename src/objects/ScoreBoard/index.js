e1ba17fcf9c8e448739058e897018d7d4a6897b6
Fernando Tavares <nando.frontend@gmail.com>
2021-08-02 20:05:47 -0300

    :tada: Refinamento do código

--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,13 +2,14 @@
 Posicionamento dos Players
  */
 import PlayerName from "../../components/PlayerName";
-
+import VsPlayer from "../../components/VsPlayer";
 import "./style.css";
 
 const ScoreBoard = () => {
     return /*html*/ ` 
     <header class="score-board">
      ${PlayerName("Player1")}
+     ${VsPlayer()}
      ${PlayerName("Player2")}
     </header>
   `;
